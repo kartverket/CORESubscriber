cde09a8af67d38310b51af2b84a719694885ed0e
jarle pedersen <jarle.pedersen@norconsult.com>
2019-05-22 16:09:56 +0200

    ensure config-file is writeable

--- CORESubscriber/Provider.cs
+++ CORESubscriber/Provider.cs
@@ -40,9 +40,24 @@ namespace CORESubscriber
 
         internal static void Save()
         {
+            while (!IsFileReady(ConfigFile)) { }
+
             using (var stream = File.Open(ConfigFile, FileMode.Create)) ConfigFileXml.Save(stream);
 
-            ConfigFileXml = ReadConfigFile();
+            //ConfigFileXml = ReadConfigFile();
+        }
+
+        internal static bool IsFileReady(string filename)
+        {
+            try
+            {
+                using (var inputStream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.None))
+                    return inputStream.Length > 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         internal static void ReadSettings()
